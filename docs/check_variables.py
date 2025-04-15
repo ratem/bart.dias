560e97845bd78409e655ea09bc63813703614216
ratembr <ratembr@gmail.com>
2025-04-15 11:00:30 -0300

    Unrelated information droped from docs

--- docs/check_variables.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Check if the environment variable is set
-api_key = os.environ.get("GOOGLE_API_KEY")
-if api_key:
-    print(f"GOOGLE_API_KEY is set: {api_key}")
-else:
-    print("GOOGLE_API_KEY is not set")
